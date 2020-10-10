440b6e028aa952bf57d92d18c7612c917c345625
Bartlomiej Dudek <bartek3dudek@gmail.com>
2020-10-10 14:07:33 +0200

    Running depth map on Jetson

---
+++ stream.py
@@ -0,0 +1,17 @@
+import stereo as st
+import numpy as np
+import cv2 as cv
+
+cameras = st.Cameras([0,1], transformations=[
+    (cv.cvtColor, cv.COLOR_BGR2GRAY)
+])
+cameras.to_csi_device()
+
+calibration = st.Calibration2Cams()
+calibration.load("calibrated/2cam-csi-12cm-v1.yaml")
+stereo = st.StereoVision2Cams(calibration)
+
+# cameras.stream()
+
+stream = st.DepthStream(cameras, stereo, False)
+stream.start()
