ac4ec61593b3aa675c41b271b0aa4f347b0937c4
Bartek <bartlomiej.dudek.3@gmail.com>
2020-10-14 11:51:31 +0200

    Sync files.

--- stereo/__main__.py
+++ stereo/__main__.py
@@ -2,6 +2,7 @@ import stereo as st
 import numpy as np
 import cv2 as cv
 
+
 calibration = st.Calibration2Cams()
 calibration.load("calibrated/2cam-usb-12cm-v1.yaml")
 
@@ -16,3 +17,5 @@ depth = stereo.calculate_depth(cameras.frames[0], cameras.frames[1])
 cv.imshow("left", cameras.frames[0])
 cv.imshow("right", cameras.frames[1])
 cv.imshow("depth", depth)
+
+
